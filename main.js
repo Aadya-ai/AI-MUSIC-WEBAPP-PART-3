d2db7ae72eb571d1f075606f9387dc73e211a14e
Candy Crush <76581343+Aadya-ai@users.noreply.github.com>
2021-07-06 12:48:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function AI_MUSIC_1() {
+    window.location.href = "";
+}
+
+function AI_MUSIC_2() {
+    window.location.href = "";
+}
+
+function Community() {
+    window.location.href = "https://community.whitehatjr.com/profile/532d0987-b65e-4ce0-9a69-b33d4f299470";
+}
